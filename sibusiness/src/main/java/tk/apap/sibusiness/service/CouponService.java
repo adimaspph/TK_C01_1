d8dbec9385891940d41e92da5d4927a1af6063a8
Siti Noer Cholizzhah H <78061248+sitinoerc@users.noreply.github.com>
2021-12-08 23:31:23 +0800

    Feat/8 menampilkan daftar coupon creation request dan mengubah status coupon (#21)
    * feat:menambah user
    
    * feat:menampilkan daftar coupon creation request dan mengubah status coupon

--- sibusiness/src/main/java/tk/apap/sibusiness/service/CouponService.java
+++ sibusiness/src/main/java/tk/apap/sibusiness/service/CouponService.java
@@ -3,9 +3,13 @@ package tk.apap.sibusiness.service;
 import tk.apap.sibusiness.model.CouponModel;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CouponService {
         List<CouponModel> getCouponList();
         void addCoupon(CouponModel coupon, String username);
         String generateCouponCode(CouponModel couponModel);
+        List<CouponModel> getCouponCreationList();
+        void acceptRequest(Long idCoupon);
+        void deleteCoupon(Long idCoupon);
 }
