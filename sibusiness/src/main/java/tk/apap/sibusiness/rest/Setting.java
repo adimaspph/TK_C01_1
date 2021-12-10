9b24b812f1ee66a8e111371be59b3e16ca23f3a7
Siti Noer Cholizzhah H <78061248+sitinoerc@users.noreply.github.com>
2021-12-10 15:15:17 +0800

    feat: get mesin dari sifactory (#34)
    * feat:menambah user
    
    * feat:menampilkan daftar coupon creation request dan mengubah status coupon
    
    * feat: menampilkan daftar coupon creation request dan mengubah status coupon
    
    * feat: menampilkan daftar coupon creation request dan mengubah status coupon
    
    * feat: menampilkan daftar coupon creation request dan mengubah status coupon
    
    * feat: menampilkan daftar coupon creation request dan mengubah status coupon
    
    * feat: menampilkan seluruh mesin dari sifactory
    
    * feat: menampilkan seluruh mesin dari sifactory

--- sibusiness/src/main/java/tk/apap/sibusiness/rest/Setting.java
+++ sibusiness/src/main/java/tk/apap/sibusiness/rest/Setting.java
@@ -1,9 +1,7 @@
 package tk.apap.sibusiness.rest;
 
 public class Setting {
-
-//    nanti jadi url nya kelompok siretail
     final public static String siretailUrl = "https://si-retail-c01.herokuapp.com";
     final public static String siItemUrl = "https://si-item.herokuapp.com";
-
+    final public static String siFactoryUrl = "https://sifactoryc01.herokuapp.com";
 }
