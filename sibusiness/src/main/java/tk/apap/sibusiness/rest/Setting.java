67eae6a4c7d84b4151fb34a3f8d91e9d048c3618
Liza Her <csitinoer@gmail.com>
2021-12-10 15:09:43 +0800

    feat: menampilkan seluruh mesin dari sifactory

--- sibusiness/src/main/java/tk/apap/sibusiness/rest/Setting.java
+++ sibusiness/src/main/java/tk/apap/sibusiness/rest/Setting.java
@@ -2,4 +2,5 @@ package tk.apap.sibusiness.rest;
 
 public class Setting {
     final public static String siItemUrl = "https://si-item.herokuapp.com";
+    final public static String siFactoryUrl = "https://sifactoryc01.herokuapp.com";
 }
