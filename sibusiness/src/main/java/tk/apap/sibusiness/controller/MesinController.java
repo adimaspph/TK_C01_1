43597206b77feb30195166785e47271efd7e08b8
Siti Noer Cholizzhah H <78061248+sitinoerc@users.noreply.github.com>
2021-12-19 15:39:41 +0800

    Feat/12 get mesin dari sifactory (#40)
    * feat:menambah user
    
    * feat:menampilkan daftar coupon creation request dan mengubah status coupon
    
    * feat: menampilkan daftar coupon creation request dan mengubah status coupon
    
    * feat: menampilkan daftar coupon creation request dan mengubah status coupon
    
    * feat: menampilkan daftar coupon creation request dan mengubah status coupon
    
    * feat: menampilkan daftar coupon creation request dan mengubah status coupon
    
    * feat: menampilkan seluruh mesin dari sifactory
    
    * feat: menampilkan seluruh mesin dari sifactory
    
    * feat: dropdown fitur 12
    
    * feat: dropdown fitur 12

--- sibusiness/src/main/java/tk/apap/sibusiness/controller/MesinController.java
+++ sibusiness/src/main/java/tk/apap/sibusiness/controller/MesinController.java
@@ -4,8 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import tk.apap.sibusiness.service.MesinRestService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/mesin")
@@ -17,6 +22,21 @@ public class MesinController {
     @GetMapping("/viewall")
     private String viewAllMesin(Model model) {
         model.addAttribute("listMesin", mesinRestService.getAllMesin());
+        return "viewall-mesin";
+    }
+
+    @GetMapping("/view/kategori/{idKategori}")
+    private String viewMesinByKategori(@PathVariable Integer idKategori, Model model) {
+        List<Map<String, Object>> data = new ArrayList<>(mesinRestService.getAllMesin());
+        List<Map<String, Object>> newdata = new ArrayList<>();
+
+        for (int i = 0; i < data.size(); i++) {
+            if (data.get(i).get("idKategori").equals(idKategori)) {
+                newdata.add(data.get(i));
+            }
+        }
+
+        model.addAttribute("listMesin", newdata);
         return "viewall-mesin";
     }
 }
